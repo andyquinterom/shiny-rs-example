11c83eb248f7651c32f88810f14974e09fecccd4
andres <afquinteromoreano@gmail.com>
2022-09-22 19:44:31 -0500

    refactor: Adapts backend to new schema

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,14 @@
 #![feature(proc_macro_hygiene)]
 #![allow(unused_braces)]
-use actix_files::NamedFile;
+use actix_files::Files;
 use actix_web::{
     web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
 };
 mod server;
 mod plot;
 use server::create_server;
+use shiny_rs::html;
 
-async fn index() -> impl Responder {
-    NamedFile::open_async("./static/index.html").await.unwrap()
-}
 
 async fn server1(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
     actix_web_actors::ws::start(create_server(), &req, stream)
@@ -20,9 +18,9 @@ async fn server1(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse,
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(web::resource("/").to(index))
-            .service(actix_files::Files::new("/lib", "./static/lib").show_files_listing())
+            .service(Files::new("/lib", "./static").show_files_listing())
             .service(web::resource("/websocket/").route(web::get().to(server1)))
+            .service(Files::new("/", "./dist").index_file("index.html"))
     })
     .workers(2)
     .bind(("0.0.0.0", 8080))?
